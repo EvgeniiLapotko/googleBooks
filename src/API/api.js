0d11b8ab4727b632e89e16aac4fc00b1bbb76ba6
Evgenii Lapotko <lapotko.e2@gmail.com>
2021-08-15 23:15:30 +0300

    start project

---
+++ src/API/api.js
@@ -0,0 +1,3 @@
+const keyAPI = "AIzaSyBMrcf7kAfixlZLo9eh2CH_hcjHpodt6_k";
+const googleLinkBook =
+    "https://www.googleapis.com/books/v1/volumes?q=javascript&maxResults=40&key=AIzaSyBMrcf7kAfixlZLo9eh2CH_hcjHpodt6_k";
