388a3439f47de7a605005b3a51b48ebe8803fb43
Evgenii Lapotko <lapotko.e2@gmail.com>
2021-08-17 20:07:09 +0300

    PC version

--- src/API/api.js
+++
@@ -1,3 +0,0 @@
-const keyAPI = "AIzaSyBMrcf7kAfixlZLo9eh2CH_hcjHpodt6_k";
-const googleLinkBook =
-    "https://www.googleapis.com/books/v1/volumes?q=javascript&maxResults=40&key=AIzaSyBMrcf7kAfixlZLo9eh2CH_hcjHpodt6_k";
